edca4b93344179e1d62de506619f7c012eea4ccd
pboot31 <pboot7146@gmail.com>
2025-02-02 21:31:16 -0500

    pboot31.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/pboot31.py
@@ -0,0 +1,24 @@
+#https://www.python.org/
+
+#comentario 1 linea
+
+'''
+comentario en 
+varias
+lineas
+'''
+
+"""
+comentario tambien en varias
+lineas 
+"""
+
+my_variable = "hola"
+
+my_int = 17 #numeros enteros
+my_float = 76.9 #numeros decimales
+my_booltrue = True #booleano verdadero
+my_boolfalse = False #booleano falso
+my_str = "esto es un nuevo texto"
+
+print("¡Hola, python!")
